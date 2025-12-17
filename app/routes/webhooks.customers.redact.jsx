6e5df5267b197b8c85ad8374804e1325e9a2e095
Srishti Verma <srishtiverma358@gmail.com>
2025-12-17 20:51:55 +0530

    added webhooks file

--- app/routes/webhooks.customers.redact.jsx
+++ app/routes/webhooks.customers.redact.jsx
@@ -1,6 +1,8 @@
-import { authenticate } from "../shopify.server";
+export const runtime = "nodejs";
+
+import { unauthenticated } from "../shopify.server";
 
 export const action = async ({ request }) => {
-  await authenticate.webhook(request);
-  return new Response();
+  await unauthenticated.webhook(request);
+  return new Response(null, { status: 200 });
 };
