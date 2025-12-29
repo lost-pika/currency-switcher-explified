cfed7bfedb1fac0bfa889aba883d7b62cff5a312
Srishti Verma <srishtiverma358@gmail.com>
2025-12-29 22:12:14 +0530

    fix: remove any default export from resource route

--- app/routes/api.merchant-settings.ts
+++ app/routes/api.merchant-settings.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../../db.server";
+import { prisma } from "../db.server";
 import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
 
 export async function loader({ request }: LoaderFunctionArgs) {
