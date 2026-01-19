8d6349744f130b3309d5962af6d03e2de7f73f14
Srishti Verma <srishtiverma358@gmail.com>
2026-01-19 20:10:24 +0530

    removing additional pages

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -103,10 +103,7 @@ function CurrencySelector({
       <div className="max-w-4xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between gap-5 items-start md:items-end mb-8 px-2">
           <div className="flex flex-col">
-            <div className="flex items-center gap-1.5 text-xs text-gray-500 mb-1 cursor-pointer">
-              <span className="text-xl">←</span>
-              <span className="hover:underline">Settings</span>
-            </div>
+            
             <h1 className="text-3xl font-semibold text-gray-800">
               Auto Currency Converter
             </h1>
@@ -471,7 +468,12 @@ function PlacementSelector({
         {/* header */}
         <div className="flex flex-col md:flex-row justify-between gap-5 items-start md:items-end mb-8 px-2">
           <div className="flex flex-col">
-            
+            <div className="flex items-center gap-1.5 text-xs text-gray-500 mb-1 cursor-pointer">
+              <span className="text-xl">←</span>
+              <span className="hover:underline" onClick={onBack}>
+                Settings
+              </span>
+            </div>
             <h1 className="text-3xl font-semibold text-gray-800">
               Auto Currency Converter
             </h1>
